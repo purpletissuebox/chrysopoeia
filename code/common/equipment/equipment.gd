1b4a0efedbad892df7e2398ef3f0c1590baa08d7
purple tissue box <dirksenmatthew1@gmail.com>
2024-07-21 10:33:15 -0400

    initial draft for how equips work. TODO export texture resource instead of inherting sprite3d

---
+++ code/common/equipment/equipment.gd
@@ -0,0 +1,6 @@
+class_name Equipment extends Sprite3D
+
+@export var armor_class: int
+@export var element: Constants.ELEMENTS
+@export var slot: Constants.EQUIP_SLOTS
+@export var listeners: Array[String]
