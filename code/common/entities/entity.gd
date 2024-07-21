50cce4003bfe2ca6317a2e332bce5c085427727d
purple tissue box <dirksenmatthew1@gmail.com>
2024-07-21 10:28:38 -0400

    added driver for automatically hooking up equipment events

--- code/common/entities/entity.gd
+++ code/common/entities/entity.gd
@@ -6,8 +6,25 @@ class_name Entity extends CharacterBody3D
 var current_health: int
 @export var max_mana: int
 var current_mana: int
-
 var position_xz: Vector2
+
+var signals_in_use: Array[String]
+
+func broadcast(event: String, args: Array):
+	if event in signals_in_use:
+		self.emit_signal(event, args)
+
+func equip(item: Equipment):
+	for sig in item.listeners:
+		if sig not in signals_in_use:
+			self.add_user_signal(sig)
+			signals_in_use.append(sig)
+		self.connect(sig, Callable(item, sig + "_triggered"))
+		self.add_child(item)
+		
+func unequip(item: Equipment):
+	self.remove_child(item)
+	item.queue_free()
 
 func _aim_at_point():
 	pass
