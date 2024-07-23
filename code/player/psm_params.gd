fccc0864b72d5ca7871417d10ac1fd2a92763f84
purple tissue box <dirksenmatthew1@gmail.com>
2024-07-23 18:16:38 -0400

    Tweaking movement values.

--- code/player/psm_params.gd
+++ code/player/psm_params.gd
@@ -1,10 +1,10 @@
 class_name PSMParams extends Resource
 
 @export var gravity = 7.0
-@export var acceleration = 50.0
+@export var acceleration = 40.0
 @export var top_speed = 4.0
 @export var jump_force = 4.0
-@export var fall_threshold = -0.5
+@export var fall_threshold = 0.75
 @export var stuntimer = 0.3
-@export var friction = 5.0
+@export var friction = 20.0
 @export var skid_threshold = 0.1
