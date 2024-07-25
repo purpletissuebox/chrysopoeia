8ea3d6c6ffb32db04808c5ea35697f5ec837089a
Harrison Wine <harrisonmwine@protonmail.com>
2024-07-25 18:16:22 -0400

    assign direction not add

--- code/common/entities/enemy-proto.gd
+++ code/common/entities/enemy-proto.gd
@@ -3,9 +3,13 @@ class_name Enemy extends Entity
 @export var params:PSMParams
 
 @onready var p_state_machine = $PStateMachine
-@onready var player_ref = get_tree().get_root().get_node("res://scenes/actors/player/player.tscn")
+@onready var player_ref = get_parent().get_node("Player")
 
 func _get_movement_direction():
-	var result:Vector3 = Vector3.ZERO
-	result += position.direction_to(player_ref.position)
+	var result:Vector2 = Vector2.ZERO
+	
+	if player_ref != null:
+		result.x = sign(position.direction_to(player_ref.position).x)
+		result.y = sign(position.direction_to(player_ref.position).y)
+
 	return result
