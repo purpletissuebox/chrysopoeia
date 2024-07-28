c7f5861481ea6cd8bb9032a639be7aabb61669fa
DesktopCoder1407 <>
2024-07-27 22:14:59 -0400

    Added working volume slider.

--- code/options/options.gd
+++ code/options/options.gd
@@ -1,7 +1,16 @@
 extends Control
 
+@onready var slider = $VolumeOption/Slider
+@onready var edit = $VolumeOption/Edit
+
 func _on_ready():
-	SoundDriver.overall_bgm_volume = 0.5
+	slider.set_value_no_signal(SoundDriver.overall_bgm_volume * 100)
+	edit.set_value_no_signal(SoundDriver.overall_bgm_volume * 100)
 	
 func exit_options():
 	self.queue_free()
+
+func volume_changed(value):
+	SoundDriver.overall_bgm_volume = value / 100
+	slider.set_value_no_signal(value)
+	edit.set_value_no_signal(value)
