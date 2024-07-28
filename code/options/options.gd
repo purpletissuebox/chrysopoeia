fa5910a6e34ef4ddb5ba9224d31b0df3467e9dbf
DesktopCoder1407 <>
2024-07-27 23:16:22 -0400

    Added SFX slider. Finalized menu look.

--- code/options/options.gd
+++ code/options/options.gd
@@ -1,16 +1,27 @@
 extends Control
 
-@onready var slider = $VolumeOption/Slider
-@onready var edit = $VolumeOption/Edit
+@onready var music_slider = $HBoxContainer/VBoxContainer/MusicVolumeOption/Slider
+@onready var music_edit = $HBoxContainer/VBoxContainer/MusicVolumeOption/Edit
+
+@onready var sfx_slider = $HBoxContainer/VBoxContainer/SFXVolumeOption/Slider
+@onready var sfx_edit = $HBoxContainer/VBoxContainer/SFXVolumeOption/Edit
+
 
 func _on_ready():
-	slider.set_value_no_signal(SoundDriver.overall_bgm_volume * 100)
-	edit.set_value_no_signal(SoundDriver.overall_bgm_volume * 100)
+	music_slider.set_value_no_signal(SoundDriver.overall_bgm_volume * 100)
+	music_edit.set_value_no_signal(SoundDriver.overall_bgm_volume * 100)
+	sfx_slider.set_value_no_signal(SoundDriver.overall_sfx_volume * 100)
+	sfx_edit.set_value_no_signal(SoundDriver.overall_sfx_volume * 100)
 	
 func exit_options():
 	self.queue_free()
 
-func volume_changed(value):
+func music_volume_changed(value):
 	SoundDriver.overall_bgm_volume = value / 100
-	slider.set_value_no_signal(value)
-	edit.set_value_no_signal(value)
+	music_slider.set_value_no_signal(value)
+	music_edit.set_value_no_signal(value)
+
+func sfx_volume_changed(value):
+	SoundDriver.overall_sfx_volume = value / 100
+	sfx_slider.set_value_no_signal(value)
+	sfx_edit.set_value_no_signal(value)
