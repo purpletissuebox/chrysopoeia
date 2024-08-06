62fdbd6e681e2a248e21e152b7ad09ca9c6f04a9
Harrison Wine <harrisonmwine@protonmail.com>
2024-08-06 15:52:05 -0400

    modify enemy by rming redundant code & able to be instanced

--- code/common/entities/enemy-proto.gd
+++ code/common/entities/enemy-proto.gd
@@ -1,30 +1,19 @@
 class_name Enemy extends Entity
 
 @export var params:PSMParams
-@export var helm:PackedScene
-@export var armor:PackedScene
-@export var boots:PackedScene
-@export var weap:PackedScene
 
 @onready var p_state_machine = $PStateMachine
-@onready var player_ref = get_parent().get_node("Player")
-
-var put_clothes_on : bool = false
-
-func setup_equipment():
-	equip(helm.instantiate())
-	equip(armor.instantiate())
-	equip(boots.instantiate())
-	equip(weap.instantiate())
-	put_clothes_on = true
+@onready var player_ref = get_tree().root.find_child("Player", true, false)
 
 func _get_movement_direction():
-	var result:Vector2 = Vector2.ZERO
-	if !put_clothes_on:
-		setup_equipment()
+	var result:Vector2 = Vector2.ZERO#
 
 	if player_ref != null:
 		result.x = position.direction_to(player_ref.position).x
 		result.y = position.direction_to(player_ref.position).z
-	
+	else:
+		player_ref = find_player()
 	return result
+
+func find_player():
+	return get_tree().root.find_child("Player", true, false)
